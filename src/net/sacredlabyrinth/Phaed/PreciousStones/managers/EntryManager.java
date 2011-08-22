19e570db6ba23d4aebca3bcffd1bb6f90eadbce9
Marcelo Delgado <phaedrus.one@gmail.com>
2011-08-22 03:39:06 -0400

    5.5.4

--- src/net/sacredlabyrinth/Phaed/PreciousStones/managers/EntryManager.java
+++ src/net/sacredlabyrinth/Phaed/PreciousStones/managers/EntryManager.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.entity.Player;
 
