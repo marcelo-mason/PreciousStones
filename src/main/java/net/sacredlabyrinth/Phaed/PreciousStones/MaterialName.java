710efb1923112629a690f8ad854557e9fa169429
Nathan Wolf <nathan@elmakers.com>
2018-08-01 16:59:31 -0700

    Allow material ids in config files, but nag about it.

--- src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/MaterialName.java
+++ src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/MaterialName.java
@@ -2,6 +2,8 @@ package net.sacredlabyrinth.Phaed.PreciousStones;
 
 import org.bukkit.Material;
 
+import net.sacredlabyrinth.Phaed.PreciousStones.helpers.Helper;
+
 public class MaterialName {
     
     private MaterialName() {
@@ -17,17 +19,14 @@ public class MaterialName {
         try {
             return Material.valueOf(IDName.toUpperCase());
         } catch (Exception ex) {
-            PreciousStones.getLog().warning("Invalid material name: " + IDName);
-        }
-        return null;
-    }
-
-    public static Material getItemMaterial(String IDName) {
-        if (IDName.startsWith("minecraft:")) IDName = IDName.substring(10);
-        try {
-            return Material.valueOf(IDName.toUpperCase());
-        } catch (Exception ex) {
-            PreciousStones.getLog().warning("Invalid material name: " + IDName);
+            try {
+                int materialId = Integer.parseInt(IDName);
+                Material converted = Helper.getMaterial(materialId);
+                PreciousStones.getLog().warning("Found material id '" + materialId + "' in config. Assuming this means " + converted.name() + " but you should update your configs.");
+                return converted;
+            } catch (Exception notint) {
+                PreciousStones.getLog().warning("Invalid material name: " + IDName);
+            }
         }
         return null;
     }
