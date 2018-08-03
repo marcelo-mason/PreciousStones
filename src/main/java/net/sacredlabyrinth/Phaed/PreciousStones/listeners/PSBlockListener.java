91f06964b165473943a7e41336255a45749c1757
Nathan Wolf <nathan@elmakers.com>
2018-08-03 08:37:54 -0700

    Give other plugins a chance to react to protected block breaks. I am worried this may regress #1088 but it's put me in a tight spot.

--- src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/listeners/PSBlockListener.java
+++ src/main/java/net/sacredlabyrinth/Phaed/PreciousStones/listeners/PSBlockListener.java
@@ -286,7 +286,7 @@ public class PSBlockListener implements Listener {
     /**
      * @param event
      */
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGH)
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.isCancelled()) {
             return;
